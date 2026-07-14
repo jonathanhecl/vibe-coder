65a2bb5749de937ed4dcff8629af5011d5c9eaa2
Jonathan Hecl <jonathanhecl@gmail.com>
2026-07-14 20:29:45 -0300

    feat: remove unused input handle configuration from Windows terminal setup
    - Remove ENABLE_VIRTUAL_TERMINAL_INPUT flag setup for input handle
    - Mark input file parameter as unused with underscore
    - Simplify terminal mode restoration to only reset output handle
    - Remove inputMode variable and associated GetConsoleMode/SetConsoleMode calls

--- internal/tui/terminal_windows.go
+++ internal/tui/terminal_windows.go
@@ -8,23 +8,17 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-func configureTerminalForBracketedPaste(in, out *os.File) (func(), bool) {
-	inputHandle := windows.Handle(in.Fd())
+func configureTerminalForBracketedPaste(_ *os.File, out *os.File) (func(), bool) {
 	outputHandle := windows.Handle(out.Fd())
 
-	var inputMode, outputMode uint32
-	if windows.GetConsoleMode(inputHandle, &inputMode) != nil || windows.GetConsoleMode(outputHandle, &outputMode) != nil {
-		return nil, false
-	}
-	if windows.SetConsoleMode(inputHandle, inputMode|windows.ENABLE_VIRTUAL_TERMINAL_INPUT) != nil {
+	var outputMode uint32
+	if windows.GetConsoleMode(outputHandle, &outputMode) != nil {
 		return nil, false
 	}
 	if windows.SetConsoleMode(outputHandle, outputMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING) != nil {
-		_ = windows.SetConsoleMode(inputHandle, inputMode)
 		return nil, false
 	}
 	return func() {
-		_ = windows.SetConsoleMode(inputHandle, inputMode)
 		_ = windows.SetConsoleMode(outputHandle, outputMode)
 	}, true
 }
